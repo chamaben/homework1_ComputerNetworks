dfed047e31c3791d3dd6eb4830003d3bf3ce34c5
Catuja <catujasmit@hotmail.com>
2022-03-22 15:58:37 +0900

    alligned server with client and deleted test

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 12345  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT)) #bind socket to address
-    s.listen() #Enable a server to accept connections
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT)) #bind socket to address
+    sock.listen() #Enable a server to accept connections
     #get socket, return address
-    conn, addr = s.accept() #
+    conn, addr = sock.accept() #
     with conn:
         print(f"Server connected to {addr}")
         while True:
