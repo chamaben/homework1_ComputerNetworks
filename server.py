a71c3f80d4a84f14a398e5dc61892833dada6113
alessandra.masur <alessandra.masur@student.tugraz.at>
2022-03-22 16:16:42 +0900

    format stuff

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 12345  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT)) #bind socket to address
-    sock.listen() #Enable a server to accept connections
-    #get socket, return address
-    conn, addr = sock.accept() #
+    sock.bind((HOST, PORT)) # bind socket to address
+    sock.listen() # enable a server to accept connections
+    # get socket, return address:
+    conn, addr = sock.accept()
     with conn:
         print(f"Server connected to {addr}")
         while True:
@@ -15,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             data = conn.recv(128)
             if not data:
                 break
-            # print received data
+            # print received data:
             print(f"Received {data!r}")
             # send data to socket:
             conn.sendall(data)
